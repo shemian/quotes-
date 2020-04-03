a8cbabbfdda200c01a82f05c0d29ff622c2b50ff
ian <shemian092@gmail.com>
2020-04-03 09:47:46 +0300

    intial changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ParticlesModule } from "angular-particle";
+import { TimeAgoPipe } from "time-ago-pipe";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuoteComponent } from "./quote/quote.component";
+import { QuoteDetailComponent } from "./quote-detail/quote-detail.component";
+import { StrikethroughDirective } from "./strikethrough.directive";
+import { DateCountPipe } from "./date-count.pipe";
+import { QuoteFormComponent } from "./quote-form/quote-form.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    QuoteFormComponent,
+    TimeAgoPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ParticlesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, HostListener } from "@angular/core";
+
+@Directive({
+  selector: "[appStrikethrough]"
+})
+export class StrikethroughDirective {
+  constructor(private elem: ElementRef) {}
+
+  @HostListener("click") onClicks() {
+    this.textDeco("overline");
+  }
+  @HostListener("dblclick") onDoubleClicks() {
+    this.textDeco("none");
+  }
+
+  private textDeco(action: string) {
+    this.elem.nativeElement.style.textDecoration = action;
+  }
+}
